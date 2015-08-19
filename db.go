a3766f42e5c254d95d1483a5f28f48d271af51fc
e.lobodin <e.lobodin@404-group.com>
2015-08-19 10:25:15 +0300

    added mapstructure

--- db.go
+++ db.go
@@ -10,7 +10,7 @@ import (
 	"sync"
 	"time"
 	"unicode"
-	//"github.com/mitchellh/mapstructure"
+	"github.com/mitchellh/mapstructure"
 )
 
 type DBMediator interface {
@@ -36,6 +36,7 @@ type Record interface {
 	GetInString(key string) string
 	Each(func(key string, value reflect.Value))
 	Merge(r Record)
+	Model(dst interface{}) error
 }
 
 type record struct {
@@ -190,12 +191,24 @@ func (r *record) Merge(r2 Record) {
 	})
 }
 
-func normalizeValue(value reflect.Value) reflect.Value {
-	k := value.Kind()
-	if k == reflect.Interface || k == reflect.Ptr {
-		return normalizeValue(value.Elem())
+func (r *record) Model(rawVal interface{}) error {
+	if r == nil {
+		return nil
 	}
-	return value
+	r.m.RLock()
+	defer r.m.RUnlock()
+
+	return newModel(r.raw, rawVal, func(in, out reflect.Type, data interface{}) (interface{}, error) {
+        dataMap, ok := data.(map[string]reflect.Value)
+        if !ok {
+            return data, nil
+        }
+        ret := make(map[string]interface{}, len(dataMap))
+        for key, val := range dataMap {
+            ret[key] = val.Interface()
+        }
+        return ret, nil
+    })
 }
 
 func (db *DB) ExistsRecord(query string, args ...interface{}) error {
@@ -210,29 +223,50 @@ func (db *DB) ExistsRecord(query string, args ...interface{}) error {
 	return nil
 }
 
-func newRecord(rows *sql.Rows, cols []string) Record {
-	/*
-		ptrs := make([]interface{}, len(cols))
-		cont := make([]string, len(cols))
-		for i, _ := range ptrs {
-			ptrs[i] = &cont[i]
-		}
-		rows.Scan(ptrs...)
-	*/
-	pointers := make([]interface{}, len(cols))
-	container := make(map[string]interface{}, len(cols))
-	for i, _ := range pointers {
-		var v interface{}
-		container[cols[i]] = &v
-		pointers[i] = &v
+func (db *DB) QueryModel(query string, model interface{}, args ...interface{}) error {
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		return err
 	}
-	rows.Scan(pointers...)
+	defer rows.Close()
+	if !rows.Next() {
+		return sql.ErrNoRows
+	}
 
-	rec := record{raw: make(map[string]reflect.Value, len(cols))}
-	for key, value := range container {
-		rec.raw[key] = reflect.Indirect(reflect.ValueOf(value)).Elem()
+	cols, err := rows.Columns()
+	if err != nil {
+		return err
 	}
-	return &rec
+    container := newContainer(rows, cols)
+	return newModel(container, model, func(in, out reflect.Type, data interface{}) (interface{}, error) {
+        dataMap, ok := data.(map[string]interface{})
+        if !ok {
+            return data, nil
+        }
+        for key, val := range dataMap {
+            //dataMap[key] = *(*interface{})(&val)
+            dataMap[key] = reflect.ValueOf(val).Elem().Interface()
+        }
+        return dataMap, nil
+    })
+}
+
+func (db *DB) QueryRecords(query string, args ...interface{}) ([]Record, error) {
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	ret := make([]Record, 0, 10)
+	cols, err := rows.Columns()
+	if err != nil {
+		return ret, err
+	}
+	for rows.Next() {
+		ret = append(ret, newRecord(rows, cols))
+	}
+	return ret, err
 }
 
 func (db *DB) QueryRecord(query string, args ...interface{}) (Record, error) {
@@ -245,21 +279,63 @@ func (db *DB) QueryRecord(query string, args ...interface{}) (Record, error) {
 		return nil, sql.ErrNoRows
 	}
 
-	cols, _ := rows.Columns()
-	return newRecord(rows, cols), err
-}
-
-func (db *DB) QueryRecords(query string, args ...interface{}) ([]Record, error) {
-	rows, err := db.Query(query, args...)
+	cols, err := rows.Columns()
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
+	return newRecord(rows, cols), err
+}
 
-	cols, _ := rows.Columns()
-	ret := make([]Record, 0, 10)
-	for rows.Next() {
-		ret = append(ret, newRecord(rows, cols))
+func normalizeValue(value reflect.Value) reflect.Value {
+	k := value.Kind()
+	if k == reflect.Interface || k == reflect.Ptr {
+		return normalizeValue(value.Elem())
 	}
-	return ret, err
+	return value
+}
+
+func newContainer(rows *sql.Rows, cols []string) map[string]interface{} {
+	/*
+		ptrs := make([]interface{}, len(cols))
+		cont := make([]string, len(cols))
+		for i, _ := range ptrs {
+			ptrs[i] = &cont[i]
+		}
+		rows.Scan(ptrs...)
+        return cont
+	*/
+	pointers := make([]interface{}, len(cols))
+	container := make(map[string]interface{}, len(cols))
+	for i, _ := range pointers {
+		var v interface{}
+		container[cols[i]] = &v
+		pointers[i] = &v
+	}
+	rows.Scan(pointers...)
+    return container
+}
+
+func newRecord(rows *sql.Rows, cols []string) Record {
+    container := newContainer(rows, cols)
+	rec := record{raw: make(map[string]reflect.Value, len(cols))}
+	for key, value := range container {
+		rec.raw[key] = reflect.Indirect(reflect.ValueOf(value)).Elem()
+	}
+	return &rec
+}
+
+func newModel(src, dst interface{}, decodeHook mapstructure.DecodeHookFuncType) error {
+	config := &mapstructure.DecoderConfig{
+        DecodeHook:       decodeHook,
+		Metadata:         nil,
+		Result:           dst,
+		WeaklyTypedInput: true,
+	}
+
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return err
+	}
+
+	return decoder.Decode(src)
 }
